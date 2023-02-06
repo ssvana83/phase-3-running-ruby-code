d7432c3a1ab9592b9f43fe8b2625060282bd9608
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-02-06 10:45:47 -0500

    commit tree 8a5a813802cc139bd31c9edc789ff66c9712ce3b

--- app.rb
+++ app.rb
@@ -14,10 +14,20 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3) + add(2,2)
+
+puts "Hello World!"
+# p ["Hello world!", "Hello world!"]
+# puts [1, 2, 3]
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
+p [1, 2, 3]
